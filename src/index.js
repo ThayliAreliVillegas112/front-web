4463e38741074f810ce3da1908ed80bf8b8825f4
IsaacGalindoBarbina <20203tn019@utez.edu.mx>
2023-12-14 13:55:09 -0600

    service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter  } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './assets/base.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import Main from './DemoPages/Main';
@@ -8,21 +8,28 @@ import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-
 const store = configureStore();
 const rootElement = document.getElementById('root');
 
-const renderApp = Component => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <BrowserRouter >
-        <Component />
-      </BrowserRouter >
-    </Provider>,
-    rootElement
-  );
+const renderApp = (Component) => {
+	ReactDOM.render(
+		<Provider store={store}>
+			<BrowserRouter>
+				<Component />
+			</BrowserRouter>
+		</Provider>,
+		rootElement
+	);
 };
-serviceWorkerRegistration.register();
+
+serviceWorkerRegistration.register({
+	onUpdate: async (registration) => {
+		if (registration && registration.waiting) {
+			await registration.unregister();
+			registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+
+			window.location.reload();
+		}
+	},
+});
 renderApp(Main);
-
-
