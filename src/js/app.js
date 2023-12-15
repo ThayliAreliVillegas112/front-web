61ca56b707df7fa403e42e53a4ffac2fd61d3516
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2023-12-14 21:00:33 -0600

    update app.ja

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,25 @@
-
-console.log('APPJS');
 const url = window.location.href;
-const API = `https://localhost:3000/api`;
+
+// Determine protocol based on environment
+const protocol =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_PROTOCOL_PROD
+    : process.env.REACT_APP_API_PROTOCOL_DEV;
+
+// Construct API base URL
+const API = `${protocol}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/${process.env.REACT_APP_API_CONTEXT}`;
+
 let swLocation = '/src/service-worker.js';
-if(navigator.serviceWorker){
-    // Register the service worker
-    if(url.includes('localhost') || url.includes('127.0.0.1')){
-        swLocation = '/service-worker.js';
-    }
 
-    // navigator.serviceWorker.register('sw.js');
-    window.addEventListener('load',()=>{
-        navigator.serviceWorker.register(swLocation).then((reg)=>{
-            //code
-        })
-    })
-} 
+if (navigator.serviceWorker) {
+  // Register the service worker
+  if (url.includes('localhost') || url.includes('127.0.0.1')) {
+    swLocation = '/service-worker.js';
+  }
 
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(swLocation).then((reg) => {
+      //code
+    });
+  });
+}
