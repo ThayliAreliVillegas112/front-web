3c58003dce52a204f590e8f80443bd2a8171cb1d
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2023-12-14 20:43:00 -0600

    update

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,11 @@
 console.log('APPJS');
 const url = window.location.href;
 const API = `https://localhost:3000/api`;
-let swLocation = '/src/sw.js';
+let swLocation = '/src/service-worker.js';
 if(navigator.serviceWorker){
     // Register the service worker
     if(url.includes('localhost') || url.includes('127.0.0.1')){
-        swLocation = '/sw.js';
+        swLocation = '/service-worker.js';
     }
 
     // navigator.serviceWorker.register('sw.js');
